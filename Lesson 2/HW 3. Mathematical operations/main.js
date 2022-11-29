9f2dba04d8e9f40bc499c8b4e2bee3bb02d5f5ae
Edokatawa <krivcikeduard@gmail.com>
2022-11-29 14:09:43 +0200

    Changed let to const

--- Lesson 2/HW 3. Mathematical operations/main.js
+++ Lesson 2/HW 3. Mathematical operations/main.js
@@ -1,5 +1,5 @@
-let numb1 = prompt('Введіть перше число:', 0);
-let numb2 = prompt('Введіть друге число:', 0);
+const numb1 = prompt('Введіть перше число:', 0);
+const numb2 = prompt('Введіть друге число:', 0);
 
 alert(`//Користувач ввів ${numb1} і ${numb2} :
 • ${numb1}+${numb2}=${+numb1 + +numb2}
