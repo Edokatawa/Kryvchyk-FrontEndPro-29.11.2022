d21245d4fb1f57af65201964d2f94ae1079b1d33
Edokatawa <krivcikeduard@gmail.com>
2022-11-29 14:05:27 +0200

    Add script for Average value

---
+++ Lesson 2/HW 4. Average value/main.js
@@ -0,0 +1,5 @@
+const numb1 = prompt('Введіть перше число:', 0);
+const numb2 = prompt('Введіть друге число:', 0);
+const numb3 = prompt('Введіть третє число:', 0);
+
+alert(`Cереднє арифметичне чисел ${numb1}, ${numb2} та ${numb3} дорівнює ${(+numb1 + +numb2 + +numb3)/3}`)
