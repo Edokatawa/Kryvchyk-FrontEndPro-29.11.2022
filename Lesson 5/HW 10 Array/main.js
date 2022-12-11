52690f9e310ca6a78e11cf2b0ff7af985aaa8cac
Edokatawa <krivcikeduard@gmail.com>
2022-12-12 01:22:44 +0200

    Fixed sort array

--- Lesson 5/HW 10 Array/main.js
+++ Lesson 5/HW 10 Array/main.js
@@ -3,10 +3,10 @@ let lengthArr;
 
 do {
     lengthArr = +prompt(`Введіть довжину масиву:`);
-    if ((!!lengthArr) || (lengthArr === 0)) {
+    if (!!lengthArr || lengthArr === 0) {
         if (lengthArr > 0) {
-            for(let i = 0; i < lengthArr; i++) {
-                let elementArr = +prompt(`Введіть ${i+1} елемент масиву:`);
+            for (let i = 0; i < lengthArr; i++) {
+                const elementArr = +prompt(`Введіть ${i + 1} елемент масиву:`);
                 arr.push(elementArr);
             }
         } else {
@@ -19,7 +19,23 @@ do {
 
 console.log(`Створений Вами масив:\n ${arr}`);
 
-console.log(`Відсортований за зростаням масив:\n ${arr.sort()}`);
+const sortedArr = arr.sort((firstElem, secondElem) => {
+    if (firstElem <= secondElem) {
+        return -1;
+    } else {
+        return 1;
+    }
+});
+console.log(`Відсортований за зростаням масив:\n ${sortedArr}`);
+const arrBeforeDeletion = sortedArr.map(elem => elem);
 
-arr.length < 4 ? console.log(`Ви не можете видалити елементи з масиву з 2 по 4(включно!), так як Ваш масив занадто маленький! :(`) : 
-console.log(`Видалені елементи з масиву з 2 по 4 (включно!):\n ${arr.splice(1,3)}`)
+if (sortedArr.length < 4) {
+    console.log(`Ви не можете видалити елементи з масиву з 2 по 4(включно!), так як Ваш масив занадто маленький! :(`);
+} else {
+    const deleteElementArr = sortedArr.splice(1,3);
+    console.log(
+        `Видалені елементи з масиву з 2 по 4 (включно!):\n ${deleteElementArr}\n`,
+        `Масив з видалиними елементами:\n ${sortedArr}\n`,
+        `Масив без видалиних елементів:\n ${arrBeforeDeletion}`
+    );
+}
