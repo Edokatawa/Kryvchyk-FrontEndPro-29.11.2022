2e14d1acdbfc1fb4a9ab4b5b160e2a68a7704244
Edokatawa <krivcikeduard@gmail.com>
2022-12-27 03:57:06 +0200

    Refactoring func getNumber, powRecursion

--- Lesson 8/HW 15 Recursive exponentiation/main.js
+++ Lesson 8/HW 15 Recursive exponentiation/main.js
@@ -3,30 +3,39 @@ console.log(
     `\n- Число, яке треба піднести до ступеню, передається як перший аргумент у функції;`,
     `\n- Ступінь передається як другий аргумент у функціюpow (num, degree).\n\n`
 );
+let errorMessage = "";
 
-const getNumber = (title) => {
-    let number = prompt(`${title}`);
+const getNumber = title => {
+    let number = prompt(`${errorMessage} ${title}`);
+    errorMessage = "";
     if (number === null) {
         return null;
     }
     number = +number;
     if (isNaN(number)) {
+        errorMessage = "Ви ввели не число!";
         return getNumber(title);
     }
 
     return number;
-}
+};
 
 const powRecursion = (num, degree) => {
-    if (degree > 1) {
-        return num * powRecursion(num, degree - 1);
-    } else {
-        return num;
+    if (typeof num === "number" && typeof degree === "number") {
+        if (degree > 1) {
+            return num * powRecursion(num, degree - 1);
+        } else {
+            return num;
+        }
     }
+};
+
+const number = getNumber("Введіть число яке потрібно піднести до ступеню:");
+const degree =
+    typeof number === "number" ? getNumber("Введіть ступінь:") : null;
+
+const pow = powRecursion(number, degree);
+
+if (typeof number === "number" && typeof degree === "number") {
+    console.log(`${number} ^ ${degree} = ${pow}`);
 }
-
-
-const number = getNumber('Введіть число яке потрібно піднести до ступеню:');
-const degree = typeof number === 'number' ?  getNumber('Введіть ступінь:') : null;
-
-console.log(`${number} ^ ${degree} = ${powRecursion(number, degree)}`);
