cd6292e11b81b58b1680f8551d283b40e983c7c5
Edokatawa <krivcikeduard@gmail.com>
2023-02-21 01:44:41 +0200

    Refactoring step 2

--- Lesson 11/HW 18 Tasks on DOM/main.js
+++ Lesson 11/HW 18 Tasks on DOM/main.js
@@ -39,9 +39,7 @@ btnOpenPrompt.addEventListener("click", () => {
     }
 });
 
-btnFollowLink.addEventListener("click", () => {
-    location.href = finalUrl;
-});
+btnFollowLink.onclick = () => (location.href = finalUrl);
 
 // 3) У папці images є зображення(додати самим) 1.jpg, 2.jpg, 3.jpg, 4.jpg, 5.jpg, 6.jpg, 7.jpg, 8.jpg, 9.jpg.
 // Вивести зображення з цієї папки отримане випадковим чином (Math.random)
